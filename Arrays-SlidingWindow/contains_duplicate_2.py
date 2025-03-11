949472c9e941ad57aeb85713863463cdc8a16425
micohrocode <michaelrocode@gmail.com>
2025-03-11 10:45:29 -0400

    contains duplicate 2

---
+++ Arrays-SlidingWindow/contains_duplicate_2.py
@@ -0,0 +1,20 @@
+# leetcoce 219
+test = [1,2,3,1]
+
+def containsNearbyDuplicate(nums, k):
+        # the sliding window
+        seen = set()
+
+        for i,num in enumerate(nums):
+            # check for duplicate
+            if num in seen:
+                return True
+            
+            seen.add(num)
+
+            # move window over
+            if len(seen) > k:
+                seen.remove(nums[i-k])
+        return False
+
+containsNearbyDuplicate(test)
