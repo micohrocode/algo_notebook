37b4fb0ccbe644ee611d4cfb3e1a1398ba4a7717
micohrocode <michaelrocode@gmail.com>
2025-03-11 10:50:48 -0400

    min abs diff

---
+++ Arrays-SlidingWindow/min_abs_diff.py
@@ -0,0 +1,19 @@
+# leetcode 1200
+test = [4,2,1,3]
+
+def minimumAbsDifference(arr):
+        # sort the array to get the right order
+        arr.sort()
+
+        # get the min difference between any two elements
+        min_diff = float('inf')
+        for i in range(1,len(arr)):
+            min_diff = min(min_diff, arr[i]-arr[i-1])
+
+        # check which pairs match that difference
+        result = []
+        for i in range(1,len(arr)):
+            if arr[i] - arr[i-1] == min_diff:
+                result.append([arr[i-1],arr[i]])
+
+        return result 
